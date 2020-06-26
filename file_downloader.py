0e2ccedcb042586545e478a15b27a80152c7bbeb
diego10ar <37212124+diego10ar@users.noreply.github.com>
2020-06-26 17:37:01 +0200

    cambios en algunas clases y config

--- file_downloader.py
+++ file_downloader.py
@@ -1,11 +1,48 @@
 import sys
 import Ice
 Ice.loadSlice('trawlnet.ice')
-import Trawlnet
-import re
+import TrawlNet
 
+
+
+class ReceiverI(Trawlnet.Receiver):
+        def start(self, current=None):
+                
+        def destroy(self, current):
+                try:
+                        current.adapter.remove(current.id)
+                        print("Receiver Destroyed"), flush=True)
+                except Exception as e:
+                        print(e, flush=True)
+
+class ReceiverFactoryI(Trawlnet.ReceiverFactory):
+        def create(string fileName, Sender* sender Transfer* transfer):
+                servant=ReceiverI()
+                proxy=current.adpter.addwithUUID(servant)
+
+                return Trawlnet.ReceiverPrx.checkedCast(proxy)
 class Client(Ice.Application):
         def run(self,argv):
+                prx='ReceiverManager.Proxy'
+                proxy=self.comunicator().propertyToProxy(prx)
+                transfer_factory=TrawlNet.TransferFactoryPrx.checkedCast(proxy)
+
+                if transfer_factory is None:
+                        raise RunTimeError("Ivalid Proxy")
+                if len(argv) < 2:
+                        raise RunTimeError("No file given")
+
+
+                broker = self.communicator()
+                servant=ReceiverFactory()
+                adapter=broker.createObjectAdapter("ReceiverFactoryAdapter")
+                prx2=adapter.add(servant,broker.stringToIdentify("receiver_factory1")
+
+                transfer=transfer_factory.newTransfer(TrawlNet.ReceiverFactoryPrx.checkedCast(prx2))
+                fileList=list(sys.argv[1:])
+                transfer.createPeers(fileList)
+
+                
                 return 0
 
 sys.exit(Client().main(sys.argv))
--- transfer_manager.py
+++ transfer_manager.py
@@ -1,5 +1,36 @@
 import sys
+import os
 import Ice
+import IceStorm
 Ice.loadSlice('trawlnet.ice')
 import Trawlnet
 
+class TransferI(TrawlNet.Transfer):
+    prxR="ReceiverFactory.Proxy"
+    prxS="SenderFactory.Proxy"
+    def crearePeers(self, files, current=None):
+        for f in files:
+            if not os.path.isfile(os.path.join(os.getcwd(),f)):
+                #cerrar todo y parar ya que uno no existe
+                
+        #si sigue la ejecucion
+        for fi in files:
+            proxyS = self.communicator().propertyToProxy(prxS)
+            sender_factory=Trawnet.SenderFactoryPrx.checkedCast(proxyS)
+            sender= sender_factory.create(fi)
+
+            proxyR = self.communicator().propertyToProxy(prxR)
+            receiver_factory=Trawnet.ReceiverFactoryPrx.checkedCast(proxyR)
+            receiver= receiver_factory.create(fi,sender, transfer)
+
+class TransferFactoryI(TrawlNet.TransferFactory):
+    def newTransfer(self,receiverFactory, current=None):
+        servant=TransferI()
+        proxy=current.adapter.addwithUUid(servant)
+
+        return TrawlNet.TransferPrx.checkedCast(proxy)
+
+class TranferManager(Ice.Application):
+    def run(self, argv):
+        
+s
